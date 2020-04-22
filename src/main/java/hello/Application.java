560fe307e40c7ac65d0798b351c18f56ae03f4b7
PSSankannavar <pooh12sept@gmail.com>
2020-04-22 20:24:49 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,18 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; 
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-  @RequestMapping("/hello")
+  @RequestMapping("/")
   public String home() {
     return "Hello Docker World";
   }
+  
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+  
+
+
+
 
 }
